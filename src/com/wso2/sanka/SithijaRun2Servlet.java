7e4c2ed4d6deaa993c36de7190bf6c2bf0a66846
SankaGIT <nuwan.ras@gmail.com>
2014-10-30 11:35:41 +0530

    initial commit

---
+++ src/com/wso2/sanka/SithijaRun2Servlet.java
@@ -0,0 +1,13 @@
+package com.wso2.sanka;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class SithijaRun2Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
